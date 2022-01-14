ae629265e1b92a9f254e5da0dc9133d5d2984995
dersanli <dersanli@gmail.com>
2022-01-14 17:47:51 +0000

    Fixed FizzBuzz

--- src/FizzBuzz.tsx
+++ src/FizzBuzz.tsx
@@ -1,10 +1,10 @@
 import React, { ReactElement } from 'react';
 
 type FizzBuzzProps = {
-    numberCount : number
+  numberCount: number
 }
 
-for (let i=1; i < 101; i++){
+for (let i = 1; i < 101; i++) {
   if (i % 15 == 0) console.log('FizzBuzz');
   else if (i % 3 == 0) console.log('Fizz');
   else if (i % 5 == 0) console.log('Buzz');
@@ -12,11 +12,11 @@ for (let i=1; i < 101; i++){
 }
 
 // TODO: write a test for this. Hint: look for `number: 15 - fizzbuzz`
-export const FizzBuzz = ({ numberCount } : FizzBuzzProps): ReactElement => {
+export const FizzBuzz = ({ numberCount }: FizzBuzzProps): ReactElement => {
   const result: ReactElement[] = [];
 
-  for(let i = 0; i < numberCount;)
-    result.push(<div>{`number: ${++i} - ${( i % 3 ? '' : 'fizz' ) + ( i % 5 ? '' : 'buzz' ) || i}`}</div>);
+  for (let i = 0; i < numberCount;)
+    result.push(<div>{`number: ${++i} - ${(i % 3 ? '' : 'fizz') + (i % 5 ? '' : 'buzz') || i}`}</div>);
 
-  return(<div>{numberCount}{result}</div>);
+  return (<div>{numberCount}{result}</div>);
 };
